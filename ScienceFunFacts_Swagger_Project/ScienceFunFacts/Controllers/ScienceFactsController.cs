113a6f889f97f902090d71c432666fe5526f1c4d
Bria <brialinza@gmail.com>
2022-08-30 23:43:26 -0600

    App.tsx update

--- ScienceFunFacts_Swagger_Project/ScienceFunFacts/Controllers/ScienceFactsController.cs
+++ ScienceFunFacts_Swagger_Project/ScienceFunFacts/Controllers/ScienceFactsController.cs
@@ -19,7 +19,8 @@ namespace ScienceFunFactsV1.Controllers
             new ScienceFact("Space is BIG!", "Did you know the universe is at least 250 larger than the observable universe?", new string[] { "https://www.space.com/24073-how-big-is-the-universe.html", "https://nineplanets.org/questions/how-big-is-the-universe/"}),
             new ScienceFact("2 is the only even prime number!", "Test it for yourself. 2 is divisible only by itself and one, all other even numbers are divisible by two.", new string[] { "https://byjus.com/questions/why-is-2-the-only-even-prime-number/"}),
             new ScienceFact("The biggest robot as of April 2022 is 28 feet tall!", "It is so big, you have to take off its head to get it out of the warehouse.", new string[] {"https://supercarblondie.com/tech/worlds-biggest-robot-28-feet-tall-shoots-sponge-balls-87mph/"}),
-            new ScienceFact("An octopus has 9 brains!", "The majority of an octopuses neurons are spread out across its tentacles! As though they have 8 for each arm.", new string[] {"https://a-z-animals.com/blog/how-many-brains-does-an-octopus-have/"})
+            new ScienceFact("An octopus has 9 brains!", "The majority of an octopuses neurons are spread out across its tentacles! As though they have 8 for each arm.", new string[] {"https://a-z-animals.com/blog/how-many-brains-does-an-octopus-have/"}),
+            new ScienceFact("Cats share 90% similarity with their brain and humans!", "A cat's cerebral cortex has about 300 million neurons, which is involved in planning action, memory, and more!", new string[] {"https://www.purina.eu/cats/behaviour-and-training/understanding-cat-behaviour/how-smart-are-cats"})
         };
 
         /// <summary>
--- science-fun-facts-client/src/App.tsx
+++ science-fun-facts-client/src/App.tsx
@@ -34,7 +34,7 @@ interface FactListProps {
 }
 const FactList: React.FC<FactListProps> = ({facts}) => {
   const factComponents = facts?.map((fact, index, facts) => (
-    <Fact factData={fact} />
+    <Fact key={index} factData={fact} />
   ));
   return (
     <div>
